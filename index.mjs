25ada66418572e0f753ea962915e6d90e65f758d
Eze <ezequiel.n.villa@gmail.com>
2024-07-19 18:06:18 -0300

    Added: command new to cli

--- index.mjs
+++ index.mjs
@@ -30,16 +30,31 @@ const questions = [
     ],
   },
 ];
+const args = process.argv.slice(2);
 
+// Check if the first argument is 'new'
+if (args[0] === "new") {
+  // Get the project name from the second argument
+  const projectName = args[1];
+
+  if (projectName) {
+    const gitCloneCommand = `git clone --depth 1 https://github.com/EzequielVilla/base-express-backend ${projectName}`;
+    const installCommand = `cd ${projectName} && npm install`;
+    console.log("Cloning repository...");
+    const checkedOut = runCommand(gitCloneCommand);
+    if (!checkedOut) process.exit(-1);
+    const installedDeps = runCommand(installCommand);
+    if (!installedDeps) process.exit(-1);
+    console.log(
+      "Project created successfully with the following configuration:"
+    );
+  } else {
+    console.log("Error: Project name is required");
+  }
+} else {
+  console.log("Error: Unknown command");
+}
 const projectName = process.argv[2];
-const gitCloneCommand = `git clone --depth 1 https://github.com/EzequielVilla/base-express-backend ${projectName}`;
-const installCommand = `cd ${projectName} && npm install`;
-console.log("Cloning repository...");
-const checkedOut = runCommand(gitCloneCommand);
-if (!checkedOut) process.exit(-1);
-const installedDeps = runCommand(installCommand);
-if (!installedDeps) process.exit(-1);
-console.log("Project created successfully with the following configuration:");
 
 inquirer
   .prompt(questions)
